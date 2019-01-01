7b3132936567c20a7c80e74a39611561ae441bee
Yesterday17 <t@yesterday17.cn>
2019-01-01 12:35:02 +0800

    Initial commit

---
+++ src/main/java/cn/yesterday17/probe/Probe.java
@@ -0,0 +1,37 @@
+package cn.yesterday17.probe;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Probe.MODID, name = Probe.NAME, version = Probe.VERSION)
+public class Probe
+{
+    public static final String MODID = "probe";
+    public static final String NAME = "Probe";
+    public static final String VERSION = "1.0.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        //
+    }
+}
