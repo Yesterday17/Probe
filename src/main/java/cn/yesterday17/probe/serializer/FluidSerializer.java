a8179e4ccba008bfbb5d94a0f335f1e1769e95a5
Yesterday17 <t@yesterday17.cn>
2019-01-13 20:03:03 +0800

    0.1.16 - Fluid id & ResourceLocation.

--- src/main/java/cn/yesterday17/probe/serializer/FluidSerializer.java
+++ src/main/java/cn/yesterday17/probe/serializer/FluidSerializer.java
@@ -4,7 +4,9 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
 
 import java.lang.reflect.Type;
 
@@ -13,8 +15,10 @@ public class FluidSerializer implements JsonSerializer<Fluid> {
     public JsonElement serialize(Fluid src, Type typeOfSrc, JsonSerializationContext context) {
         JsonObject jsonFluid = new JsonObject();
 
+        jsonFluid.addProperty("id", FluidRegistry.getRegisteredFluidIDs().get(src));
         jsonFluid.addProperty("name", src.getName());
         jsonFluid.addProperty("unlocalizedName", src.getUnlocalizedName());
+        jsonFluid.add("resourceLocation", context.serialize(new ResourceLocation(FluidRegistry.getDefaultFluidName(src))));
         jsonFluid.addProperty("luminosity", src.getLuminosity());
         jsonFluid.addProperty("density", src.getDensity());
         jsonFluid.addProperty("temperature", src.getTemperature());
@@ -25,7 +29,7 @@ public class FluidSerializer implements JsonSerializer<Fluid> {
         // jsonFluid.add("block", context.serialize(src.getBlock()));
         jsonFluid.add("still", context.serialize(src.getStill()));
         jsonFluid.add("flowing", context.serialize(src.getFlowing()));
-        
+
         return jsonFluid;
     }
 }
